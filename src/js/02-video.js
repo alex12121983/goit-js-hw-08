d523bb59e56d9879175c6e88fdf3083bd2c9be0f
Alex Tarasasenko <alex12121983@gmail.com>
2023-03-16 12:56:51 +0100

    Task #2 is done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+    const iframe = document.querySelector('iframe');
+
+    const player = new Player(iframe);
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+     const currentTime = (data) => {
+        localStorage.setItem(
+         "videoplayer-current-time",
+         JSON.stringify(data.seconds)
+        );
+    let time = Number(localStorage.getItem("videoplayer-current-time"));
+    console.log(time);
+    }
+
+    player.on('timeupdate', throttle(currentTime, 1000));
+
+    player
+    .setCurrentTime(localStorage.getItem("videoplayer-current-time"))
+    .then((seconds) => {})
+    .catch((error) => {
+        switch (error.name) {
+            case 'RangeError':
+            break;
+
+            default:
+            break;
+        }
+    })
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+const parseData = localStorage.getItem(STORAGE_KEY, JSON.parse(formData));
+
+const refs = {
+form: document.querySelector('.feedback-form'),
+email: document.querySelector('.feedback-form input[type="email"]'),
+textarea: document.querySelector('.feedback-form textarea'),
+};
+populateTextarea();
+refs.form.addEventListener("click", onFormSubmit);
+refs.email.addEventListener("input", throttle(onInput, 500));
+refs.textarea.addEventListener("input", throttle(onInput, 500));
+function onInput(evt) {
+    formData[evt.target.name] = evt.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+function onFormSubmit(evt){
+    evt.preventDefault();
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+    console.log(formData);
+};
+function populateTextarea() {
+    if (parseData) {
+        const formKeys = Object.keys(parseData);
+            formKeys.map(element => {
+                document.querySelector(`[name='${element}']`).value = parseData[element];
+            })
+    }
+}
